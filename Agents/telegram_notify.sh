24a6954eac02ac075c4ea5e63a3757f8641296c0
christer olsen <christerolsen@christers-MacBook-Pro-2.local>
2025-07-16 17:30:00 +0200

    🔁 Autosync: Wed Jul 16 17:30:00 CEST 2025

--- Agents/telegram_notify.sh
+++ Agents/telegram_notify.sh
@@ -1,7 +1,8 @@
-#!/bin/bash source ~/.vaultload.sh 
-TOKEN=$TELEGRAM_TOKEN CHAT_ID=$(cat 
-~/.telegram_chat) TEXT="$1" curl -s -X POST 
-"https://api.telegram.org/bot$TOKEN/sendMessage" 
-\
-    -d chat_id="$CHAT_ID" \
-    --data-urlencode "text=$TEXT"
+#!/bin/bash
+TOKEN=$(gpg --decrypt ~/GinieSystem/Vault/keys/telegram_token.gpg 2>/dev/null)
+CHAT_ID=$(cat ~/.telegram_chat)
+TEXT="$1"
+
+curl -s -X POST "https://api.telegram.org/bot$TOKEN/sendMessage" \
+  -d chat_id="$CHAT_ID" \
+  --data-urlencode "text=$TEXT"
