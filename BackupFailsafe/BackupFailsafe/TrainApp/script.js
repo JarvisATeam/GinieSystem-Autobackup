edfd8bd0d7403379aecbe0cbfadffdca17b43def
christer olsen <christerolsen@christers-MacBook-Pro-2.local>
2025-07-22 05:30:07 +0200

    🔒 Automatisk failsafe backup 2025-07-22 05:30:07

---
+++ BackupFailsafe/BackupFailsafe/TrainApp/script.js
@@ -0,0 +1,9 @@
+function logWorkout() {
+  document.getElementById('status').innerText = "📋 Økt logget!";
+}
+function startTrip() {
+  document.getElementById('status').innerText = "🚀 Trip aktivert!";
+}
+function backup() {
+  document.getElementById('status').innerText = "🔐 Vault-synk pågår...";
+}
